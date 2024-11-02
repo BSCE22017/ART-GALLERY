1565880407ec89aebc4133f9bd1be1e8773f5d44
bsce22018 <bsce22018@itu.edu.pk>
2024-11-02 17:51:06 +0500

    art-gallery

---
+++ art-galley-dbms-project/my-art-gallery/src/Admin/AdminHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function AdminHome() {
+  return (
+    <img height="90%" width="100%" className="admin" src='/admin1.jpg' alt='customer-portal'></img>
+  )
+
+
+}
+
+export default AdminHome;
