b3dc66affe0124dcc8321101c1a001bd1fe0419b
BSCE22017 <Bsce22017@itu.edu.pk>
2024-11-05 17:05:00 +0500

    Initial

---
+++ my-art-gallery/src/Artist/ArtistHome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ArtistHome.css';
+
+
+function ArtistHome() {
+  return (
+    <div>
+      <img className="customer-portal" src='/artisthome.jpg' alt='customer-portal'></img>
+    </div>
+  
+  )
+
+
+}
+
+export default ArtistHome;
